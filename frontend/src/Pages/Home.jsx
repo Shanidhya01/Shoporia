3c936897e107d83625254c41da76c19aeffe8941
Shanidhya01 <luckykumar0011s@gmail.com>
2025-09-28 00:18:21 +0530

    Updated Home.jsx, refactored styles, added new components and assets

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,14 +1,71 @@
 import React from 'react'
 import Footer from '../components/Footer'
+import "./Styles/Home.css"
+import Navbar from '../components/Navbar'
+import ImageSlider from '../components/ImageSlider'
+import Product from '../components/Product'
+
+const products = [
+  {
+            "_id": "68ce98c81dac062361aa77df",
+            "name": "Product1",
+            "description": "Product Description",
+            "price": 100,
+            "ratings": 0,
+            "images": [
+                {
+                    "public_id": "this is test id1",
+                    "url": "This is test url",
+                    "_id": "68ce98c81dac062361aa77e0"
+                }
+            ],
+            "category": "shirt",
+            "stock": 10,
+            "numOfReviews": 0,
+            "reviews": [],
+            "createdAt": "2025-09-20T12:06:32.860Z",
+            "__v": 0
+        },
+        {
+            "_id": "68d42e08f9bd10fc5c06712c",
+            "name": "Product401",
+            "description": "Product Description",
+            "price": 1000,
+            "ratings": 0,
+            "images": [
+                {
+                    "public_id": "this is test id1",
+                    "url": "This is test url",
+                    "_id": "68d42e08f9bd10fc5c06712d"
+                }
+            ],
+            "category": "Laptop",
+            "stock": 10,
+            "numOfReviews": 0,
+            "user": "68d14585d866bb93d2bfa5cd",
+            "reviews": [],
+            "createdAt": "2025-09-24T17:44:40.315Z",
+            "__v": 0
+        },
+]
 
 function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Shoporia
-      </h1>
-      <Footer />
-    </div>
+    <>
+      <div>
+        <Navbar />
+        <ImageSlider />
+        <div className='home-container'>
+          <h2 className='home-heading'>Welcome to Shoporia</h2>
+          <div className="home-product-container">
+            {products.map((product,index) => (
+              <Product key={index} product={product} />
+            ))}
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </>
   )
 }
 
---
+++ frontend/src/components/ImageSlider.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from 'react'
+import "./Styles/ImageSlider.css"
+
+const images = [
+  "./images/banner1.jpg",
+  "./images/banner2.jpg",
+  "./images/banner3.jpg",
+  "./images/banner6.png",
+  "./images/banner4.png",
+  "./images/banner5.png"
+]
+
+function ImageSlider() {
+
+  const [currentIndex, setCurrentIndex] = React.useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000); // Change image every 5 seconds
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className='image-slider-container'>
+      <div className="slider-images" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
+        {images.map((image, index) => (
+          <div className="slider-item" key={index}>
+            <img src={image} alt={`Banner ${index + 1}`} />
+          </div>
+        ))}
+      </div>
+
+      <div className="slider-dots">
+        {images.map((_, index) => (
+          <span className={`dot ${index === currentIndex ? 'active' : ''}`} onClick={() => setCurrentIndex(index)} key={index} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default ImageSlider
